54fe5d2304668fc6113f0d9a5572bcd539149366
Kat Matfield <katmatfield@gmail.com>
2015-07-28 11:17:29 +0100

    Make time field free text rather than integer

--- queuetimers/models.py
+++ queuetimers/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Timer(models.Model):
 	queue = models.CharField(max_length=80)
-	time = models.IntegerField(default=3)
+	time = models.CharField(max_length=10)
 
 	def __str__(self):
 		return self.queue
